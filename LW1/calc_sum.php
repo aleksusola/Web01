fb799b0a06b9f4674d484f4c1687646340a585a0
Ivanov Alexey <aleksusola@yandex.ru>
2015-04-25 01:13:49 +0300

    Update2

--- LW1/calc_sum.php
+++ LW1/calc_sum.php
@@ -1,5 +1,7 @@
 <?php
+    $arg1 = isset($_GET['arg1']);
+    $arg2 = isset($_GET['arg2']);
     $summa = $_GET["arg1"] + $_GET["arg2"];
     echo $summa;
     /* запрос: /calc_sum.php?arg1=10&arg2=15
-    */
+    */  
--- LW1/remove_extra_blanks.php
+++ LW1/remove_extra_blanks.php
@@ -1,5 +1,7 @@
 <?php
     header("Content-Type: text/plain");
     $text = trim($_GET["text"]);
-    $text = preg_replace("/\s+/"," ",$text);
+    $text = preg_replace('/ {2,}/',' ', $text);
     echo $text;
+    /* text =  1  2   3    4     5   ;
+    */
