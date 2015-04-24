f6d5c7ed532fe2020c0145340446bef609c41804
Ivanov Alexey <aleksusola@yandex.ru>
2015-04-24 21:32:47 +0300

    update

--- LW1/print_name.php
+++ LW1/print_name.php
@@ -1,3 +1,3 @@
 <?php
     header ("Content-Type: text/plain");
-    echo "Hello, Dear" . $_GET["name"] . "!"; //Запрос HTTP/1.1 GET/print_name.php?name=Sarah
+    echo 'Hello, Dear' . (isset($_GET['name']) ? $_GET['name'] : '') . '!'; //Запрос HTTP/1.1 GET/print_name.php?name=Sarah
