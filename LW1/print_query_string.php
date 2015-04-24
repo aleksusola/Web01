0918fc44eec5ec26b3a933a4e1601bc344c599fe
Ivanov Alexey <aleksusola@yandex.ru>
2015-04-24 21:13:31 +0300

    lw1

--- LW1/print_query_string.php
+++ LW1/print_query_string.php
@@ -1,6 +1,6 @@
 <?php
     header ("Content-Type: text/plain");
-    echo "Query string = '".$_SERVER["QUERY_STRING"]."'";
+    echo "Query string = '" . $_SERVER["QUERY_STRING"] . "'";
     /*  HTTP/1.1 GET/print_query_string.php?my_name=Sarah&my_surname=Rever&message=British+are+coming+by+sea.
     */
     
