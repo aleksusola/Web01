89b7d4d24c6705677625e10d2b229e8103c14666
Ivanov Alexey <aleksusola@yandex.ru>
2015-04-29 22:02:32 +0300

    calc

--- LW1/calc_sum.php
+++ LW1/calc_sum.php
@@ -1,7 +1,7 @@
 <?php
-    $arg1 = isset($_GET['arg1']);
-    $arg2 = isset($_GET['arg2']);
-    $summa = $_GET["arg1"] + $_GET["arg2"];
+    $arg1 = isset($_GET['arg1']) ? $_GET['arg1'] : '';
+    $arg2 = isset($_GET['arg2']) ? $_GET['arg2'] : '';
+    $summa = $arg1 + $arg2;
     echo $summa;
     /* запрос: /calc_sum.php?arg1=10&arg2=15
     */  
