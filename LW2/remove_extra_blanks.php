c70b78397450c675bc0feb46662176bfe1c204ee
Ivanov Alexey <aleksusola@yandex.ru>
2015-05-20 17:40:07 +0300

    Update3

--- LW2/remove_extra_blanks.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-Type: text/plain');
-    $text = isset($_GET['text']) ? $_GET['text'] : '';
-	$text = trim($_GET["text"]);
-    $text = preg_replace('/ {2,}/',' ', $text);
-    echo $text;
-    /* text =  1  2   3    4     5   ;
-    */
